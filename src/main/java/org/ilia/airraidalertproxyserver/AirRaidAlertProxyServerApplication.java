a3495d11a3c3e9c56c205c9ee40ec01b3d327478
IliaWithHat <rozhkoilia5533@gmail.com>
2024-07-14 16:19:32 +0300

    Init.

---
+++ src/main/java/org/ilia/airraidalertproxyserver/AirRaidAlertProxyServerApplication.java
@@ -0,0 +1,12 @@
+package org.ilia.airraidalertproxyserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirRaidAlertProxyServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AirRaidAlertProxyServerApplication.class, args);
+    }
+}
---
+++ src/test/java/org/ilia/airraidalertproxyserver/AirRaidAlertProxyServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ilia.airraidalertproxyserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirRaidAlertProxyServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
